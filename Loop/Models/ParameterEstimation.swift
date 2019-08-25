b23a350864254221be67ac95400ebe8f055df870
@dm61 <maksimov@colorado.edu>
2019-08-25 08:31:14 -0600

    single trailing-fasting interval assembly

--- Loop/Models/ParameterEstimation.swift
+++ Loop/Models/ParameterEstimation.swift
@@ -244,6 +244,14 @@ class ParameterEstimation {
             estimationIntervals.append(EstimationInterval(startDate: runningEndDate, endDate: self.endDate, type: .fasting, glucose: glucoseEffect, insulinEffect: insulinEffect, basalEffect: basalEffect))
         }
         
+        if estimationIntervals.count == 0 && self.endDate > self.startDate {
+            // if no intervals assembled so far, add a fasting interval between startDate and endDate
+            let glucoseFasting = self.glucose.filterDateRange(self.startDate, self.endDate)
+            let insulinEffectFasting = self.insulinEffect?.filterDateRange(self.startDate, self.endDate)
+            let basalEffectFasting = self.basalEffect?.filterDateRange(self.startDate, self.endDate)
+            estimationIntervals.append(EstimationInterval(startDate: self.startDate, endDate: self.endDate, type: .fasting, glucose: glucoseFasting, insulinEffect: insulinEffectFasting, basalEffect: basalEffectFasting))
+        }
+        
         self.parameterEstimationStatus = "*** Estimation interval assembly completed with a fasting interval. There is no active meal absorption."
         return
     }
